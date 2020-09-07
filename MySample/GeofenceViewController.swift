ce0c580ec36dafa8e2a269b669dc7504b00fa1c0
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-07 15:42:30 +0900

    変数と、アクションを追加

--- MySample/GeofenceViewController.swift
+++ MySample/GeofenceViewController.swift
@@ -2,10 +2,15 @@
 import UIKit
 
 class GeofenceViewController: UIViewController {
-
+    
+    @IBOutlet weak var dispTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    
+    @IBAction func pushGeofenceStart(_ sender: Any) {
+    }
+    
 }
